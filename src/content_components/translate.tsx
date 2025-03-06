8baa3141deb895fa5bf589b503e051ea056a148f
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2025-03-07 01:10:31 +0330

    Adding right to left formatting for Arabic and Persian languages
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- src/content_components/translate.tsx
+++ src/content_components/translate.tsx
@@ -1,30 +1,29 @@
-import React, { useEffect } from 'react';
-import styles from './translate.scss';
-import { OutputFormat, TargetLanguage, TranslateMessageType, TranslatorType, createTranslator } from '../service/translator';
-import { TranslateStore } from '../service/store';
+import React, { useEffect } from "react";
+import styles from "./translate.scss";
+import {
+  OutputFormat,
+  TargetLanguage,
+  TranslateMessageType,
+  TranslatorType,
+  createTranslator,
+} from "../service/translator";
+import { TranslateStore } from "../service/store";
 
 const Translate = (props: { inputText: string }) => {
   const [show, setShow] = React.useState<boolean>(true);
-  const [resultContent, setResultContent] = React.useState<string>('');
+  const [resultContent, setResultContent] = React.useState<string>("");
   const [showEnd, setShowEnd] = React.useState<boolean>(false);
   const [showError, setShowError] = React.useState<boolean>(false);
   const [loading, setLoading] = React.useState<boolean>(true);
+  const [isRTL, setIsRTL] = React.useState<boolean>(false);
 
-  const escapeHtml = (text: string) => {
-    return text
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#039;')
-      .replace(/\n/g, '<br />');
-  }
-
-  const handleClickClose = (e: MouseEvent | React.MouseEvent<HTMLSpanElement>) => {
+  const handleClickClose = (
+    e: MouseEvent | React.MouseEvent<HTMLSpanElement>
+  ) => {
     e.preventDefault();
     e.stopPropagation();
     setShow(false);
-  }
+  };
 
   useEffect(() => {
     const funcTranslate = async () => {
@@ -37,49 +36,66 @@ const Translate = (props: { inputText: string }) => {
       const type = settings.translatorType || TranslatorType.ChatGPT;
       const targetLang = settings.targetTransLang || TargetLanguage.English;
       const translator = createTranslator(type);
-      translator.translate(props.inputText, targetLang, OutputFormat.HTML, (message, msgType) => {
-        if(loading) {
-          setLoading(false);
+      const targetLang = settings.targetTransLang || TargetLanguage.English;
+
+      // Check if the target language is RTL
+      const rtlLanguages = [TargetLanguage.Arabic, TargetLanguage.Persian];
+      setIsRTL(rtlLanguages.includes(targetLang));
+
+      translator.translate(
+        props.inputText,
+        targetLang,
+        OutputFormat.HTML,
+        (message, type) => {
+          if (loading) {
+            setLoading(false);
+          }
+          switch (type) {
+            case TranslateMessageType.Error:
+              setShowError(true);
+              setResultContent(message);
+              break;
+            case TranslateMessageType.Message:
+              setShowError(false);
+              setResultContent(message);
+              break;
+            case TranslateMessageType.End:
+              setShowError(false);
+              setShowEnd(true);
+              break;
+          }
         }
-        switch(msgType) {
-          case TranslateMessageType.Error:
-            setShowError(true);
-            setResultContent(message);
-            setShowEnd(true);
-            break;
-          case TranslateMessageType.Message:
-            setShowError(false);
-            setResultContent(message);
-            break;
-          case TranslateMessageType.End:
-            setShowError(false);
-            setShowEnd(true);
-            break;
-        }
-      });
-    }
+      );
+    };
     funcTranslate();
-
   }, [props.inputText]);
-
 
   return (
     <>
-    { show && <div className={styles.translate}>
-      <div className={showError ? styles.error : styles.result}>
-        { loading && <div className={styles.loading}>&#9998; {chrome.i18n.getMessage("translateLoading")}</div> }
-        <div
-          dangerouslySetInnerHTML={{
-            __html: showError ? escapeHtml(resultContent) : resultContent,
-          }}
-        ></div>
-        { showEnd && <>
-          <span className={styles.close} onClick={handleClickClose}>&#x2715;</span></>
-        }
-      </div>
-    </div> }
+      {show && (
+        <div className={styles.translate}>
+          <div className={showError ? styles.error : styles.result}>
+            {loading && (
+              <div className={styles.loading}>
+                &#9998; {chrome.i18n.getMessage("translateLoading")}
+              </div>
+            )}
+            <div
+              dir={isRTL ? "rtl" : "ltr"}
+              dangerouslySetInnerHTML={{ __html: resultContent }}
+            ></div>
+            {showEnd && (
+              <>
+                <span className={styles.close} onClick={handleClickClose}>
+                  &#x2715;
+                </span>
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default Translate;
